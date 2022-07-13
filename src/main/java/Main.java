4478a72434fb6a0b99ae4f74cdcd5f9065de881e
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-12 21:57:27 -0500

    Se realiza el c, d y e del Ejercicio 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
         Exercise1 exercise1 = new Exercise1();
         //exercise1.distinct();
-        exercise1.filter();
+        //exercise1.filter();
+        //exercise1.map();
+        //exercise1.cantidadCorreos();
+        exercise1.catidadCorreosGmailHotmailOutlook();
+
     }
 
 }
--- src/main/java/exercises/Exercise1.java
+++ src/main/java/exercises/Exercise1.java
@@ -3,6 +3,7 @@ package exercises;
 import util.DataEmail;
 
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Exercise1 {
@@ -23,6 +24,39 @@ public class Exercise1 {
                 .filter(email -> email.contains("gmail") || email.contains("hotmail ") || email.contains("outlook"))
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
+    }
+
+    public void map(){
+        emails.stream()
+                .filter(email -> email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"))
+                .map(email -> email.toLowerCase())
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+
+    public void cantidadCorreos(){
+        System.out.println(emails.stream()
+                .filter(email -> email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"))
+                .count());
+    }
+
+    public void catidadCorreosGmailHotmailOutlook(){
+        System.out.println("Cantidad Correos Gmail: " + emails.stream()
+                .map(email -> email.toLowerCase())
+                .filter(email -> email.contains("gmail"))
+                .count());
+
+        System.out.println("Cantidad Correos Hotmail: " + emails.stream()
+                .map(email -> email.toLowerCase())
+                .filter(email -> email.contains("hotmail"))
+                .count());
+
+        System.out.println("Cantidad Correos Outlook: " + emails.stream()
+                .map(email -> email.toLowerCase())
+                .filter(email -> email.contains("outlook"))
+                .count());
     }
 
 }
